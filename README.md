370832ee022c9f9c61a2d35b5dc63a05869f50bc
Rémi BONNET <bonnet@fanaticalhelp.com>
2018-09-06 18:44:08 +0200

    Documentation

---
+++ README.md
@@ -0,0 +1,49 @@
+# IoT UPnP
+
+This project is a little pure Python library to annonce a device by UPnP. It's made for IoT developers to let them show their device on network discovery in any OS.
+
+With IoT-UPnP, you can show your device on all computers with Windows Explorer (network discovery). Users can doucle-click on the device to open the device configuration page. The device will be shown without any software installation on user's computers.
+
+Developers can use UPnP to announce custom services and let others applications use these services. For example, if cameras announce a service "exemple-org:service:camera-image", you can made a software witch easy discover all cameras with this service.
+
+# Requirement
+
+You need Python 3.x to use this library.
+
+# Documentation
+## Quick start
+### Installation
+You can use pip to install iot-upnp.
+
+    pip install iot-upnp
+
+### Usage
+The module is `upnp`. You need to create a device with a service and announce the device.
+
+    import upnp
+
+    device = upnp.Device()
+    device.deviceType = 'run-sadmin-fr:device:demo:1'
+    device.friendlyName = 'Demo UPnP Device'
+    device.manufacturer = 'Bontiv'
+    device.manufacturerURL = 'https://github.com/bontiv/'
+    device.Description = 'A simple device witch open the project URL on double-click'
+    device.modemName = 'DEMO-UPnP'
+    device.modelNumber = 'DEMO-1.0'
+    device.presentationURL = 'https://bontiv.github.io/iot-upnp/'
+On the device we will make a new service.
+
+    service = upnp.Service()
+    device.addService(service)
+
+Next we create the announcer. The announcer use the asyncio feature. You can use the loop on your application or just use `foreaver` to run the asyncio loop foreaver.
+It's not mandatory but I advice you tou send a NOTIFY event on the network to announce that your device is now available.
+
+     server = upnp.Annoncer(device)
+     server.initLoop()
+     server.notify()
+     server.foreaver()
+     server.dispose()
+
+## Full documentation
+The full documentation are build with sphinx. It can be found on Github page : https://bontiv.github.io/iot-upnp.
--- setup.py
+++ setup.py
@@ -2,17 +2,76 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+from os import path
 
+version = '0.1'
+release = '0.1.dev7'
+cmdclass = {}
+cmdopts = {}
+
+#Using Shinx documentation
+try:
+    from sphinx.setup_command import BuildDoc
+    import os.path
+
+    class GitlabDocs(BuildDoc):
+        def finalize_options(self):
+            super(GitlabDocs, self).finalize_options()
+            self.builder_target_dirs = [(builder, self.build_dir if builder == 'html' else os.path.join(self.build_dir, builder)) for builder in self.builder]
+            print(self.builder_target_dirs)
+
+
+    cmdclass['build_docs'] = GitlabDocs
+    cmdopts['build_docs'] = {
+        'project': ('setup.py', 'IoT-UPnP'),
+        'version': ('setup.py', version),
+        'release': ('setup.py', release),
+        'source_dir' : ('setup.py', 'docs.src'),
+        'build_dir' : ('setup.py', 'docs')
+    }
+except ImportError:
+    print("You must install Sphinx for documentation generation")
+
+#README
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+#Main setup
 setup(name='iot-upnp',
-    version='0.1',
+    version=release,
     description='UPnP device annoncement library',
     author='Remi BONNET',
     author_email='bonnet@fanaticalhelp.com',
-    url='https://sadmin.fr',
     packages=['upnp'],
+    license = 'GPLv3',
+    classifiers= [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Networking'
+    ],
+    keywords='upnp ssdp network',
+    python_requires='>3.0',
+    url='https://github.com/bontiv/iot-upnp',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
+    project_urls={
+        'Documentation': 'https://bontiv.github.io/iot-upnp/',
+        'Source': 'https://github.com/bontiv/iot-upnp',
+        'Tracker': 'https://github.com/bontiv/iot-upnp/issues'
+    },
     install_requires=[
         'ssdp',
         'netifaces',
-    ]
+    ],
+
+    extras_require={
+        'docs': ['sphinx']
+    },
+
+    cmdclass=cmdclass,
+    command_options=cmdopts
 )
