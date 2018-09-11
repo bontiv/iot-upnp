412772f741be21d1a2de978e69b08add62ad7ee7
Rémi BONNET <bonnet@fanaticalhelp.com>
2018-09-11 12:43:24 +0200

    Move setup.py from unittest suite to pytest to clear package files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 from os import path, getenv
 
-version = '1.0.2'
+version = '1.0.3'
 release = getenv('TRAVIS_TAG', '0.1.dev10')
 cmdclass = {}
 cmdopts = {}
@@ -26,47 +26,16 @@ except ImportError:
 
 #README
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
 
 with open(path.join(this_directory, 'docs', 'requirements.txt')) as f:
     docs_required = f.read().splitlines()
-print(docs_required)
 
 #Main setup
-setup(name='iot-upnp',
+setup(
     version=release,
-    description='UPnP device annoncement library',
-    author='Remi BONNET',
-    author_email='bonnet@fanaticalhelp.com',
-    packages=['upnp'],
-    license = 'GPLv3',
-    classifiers= [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-        'Programming Language :: Python :: 3',
-        'Topic :: System :: Networking'
-    ],
-    keywords='upnp ssdp network',
-    python_requires='>3.0',
-    url='https://github.com/bontiv/iot-upnp',
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-
-    project_urls={
-        'Documentation': 'https://iot-upnp.readthedocs.io/',
-        'Tracker': 'https://github.com/bontiv/iot-upnp/issues'
-    },
-    install_requires=[
-        'ssdp',
-        'netifaces',
-    ],
-
-    extras_require={
-        'docs': docs_required
-    },
-
     cmdclass=cmdclass,
-    command_options=cmdopts
+    command_options=cmdopts,
+    extras_require={
+        'docs' : docs_required
+    }
 )
